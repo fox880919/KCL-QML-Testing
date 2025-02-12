8258b00bb2e02ba95609a7adbddae48a071dc948
fayez <fox880919@gmail.com>
2025-02-12 12:51:04 +0000

    first commit

---
+++ data/data_manager.py
@@ -0,0 +1,11 @@
+from sklearn.decomposition import PCA
+
+class DataManager:
+
+      def implementPCA(self, x_tr, x_test, pca_n_components):
+
+        pca = PCA(n_components = pca_n_components)
+        xs_tr = pca.fit_transform(x_tr)
+        xs_test = pca.transform(x_test)
+
+        return xs_tr, xs_test
